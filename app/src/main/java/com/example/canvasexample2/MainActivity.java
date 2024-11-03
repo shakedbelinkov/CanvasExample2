5f7b6fe27fa7ffda57000713cdfe3b519d2d5812
noa1107 <noadachbash@gmail.com>
2024-11-03 16:11:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/canvasexample2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.canvasexample2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.ColorKt;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        MyCanvasView myCanvasView=new MyCanvasView(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void changeColor(View view) {
+        MyCanvasView myCanvasView = findViewById(R.id.canvas01);
+        myCanvasView.changeBackgroundColor(Color.GREEN);
+
+    }
+}
---
+++ app/src/main/java/com/example/canvasexample2/MyCanvasView.java
@@ -0,0 +1,125 @@
+package com.example.canvasexample2;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.core.content.res.ResourcesCompat;
+
+public class MyCanvasView extends View {
+    private Paint mPaint;
+    private Path mPath;
+    private int mDrawColor;
+    private int mBackgroundColor;
+    private Canvas mExtraCanvas;
+    private Bitmap mExtraBitmap;
+    private float mX, mY;
+    private static final float TOUCH_TOLERANCE = 4;
+    MyCanvasView(Context context, Bitmap mExtraBitmap) {
+        this(context, null, mExtraBitmap);
+    }
+
+    public MyCanvasView(Context context)
+    {
+        super(context);
+    }
+
+    public MyCanvasView(Context context, AttributeSet attributeSet)
+    {
+        super(context,attributeSet);
+        this.mExtraBitmap = mExtraBitmap;
+        mBackgroundColor = ResourcesCompat.getColor(getResources(),R.color.opaque_yellow, null);
+        mDrawColor = ResourcesCompat.getColor(getResources(),R.color.opaque_orange, null);
+
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setColor(mDrawColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+    }
+    public MyCanvasView(Context context, AttributeSet attributeSet, Bitmap mExtraBitmap) {
+        super(context);
+        this.mExtraBitmap = mExtraBitmap;
+        mBackgroundColor = ResourcesCompat.getColor(getResources(),R.color.opaque_yellow, null);
+        mDrawColor = ResourcesCompat.getColor(getResources(),R.color.opaque_orange, null);
+
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setColor(mDrawColor);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+    }
+    @Override
+    protected void onSizeChanged(int width, int height,int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        mExtraBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888);
+        mExtraCanvas = new Canvas(mExtraBitmap);
+        mExtraCanvas.drawColor(mBackgroundColor);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(mExtraBitmap, 0, 0, null);
+    }
+
+
+    public void changeBackgroundColor(int color)
+    {
+        mExtraCanvas = new Canvas(mExtraBitmap);
+        mExtraCanvas.drawColor(color);
+        invalidate();
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                break;
+            default:
+        }
+        return true;
+    }
+    private void touchStart(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+    private void touchMove(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+            mExtraCanvas.drawPath(mPath, mPaint);
+        }
+    }
+    private void touchUp() {
+        mPath.reset();
+    }
+}
